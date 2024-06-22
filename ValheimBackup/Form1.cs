9dbabef2f9087e40b188221982cd51f498b229f7
conman1005 <connercullity@live.ca>
2024-06-22 00:26:55 -0400

    Added Comments

--- ValheimBackup/Form1.cs
+++ ValheimBackup/Form1.cs
@@ -1,13 +1,27 @@
+/*
+ *              Authour: Conner Cullity
+ *               GitHub: @conman1005
+ *            Repo Link: https://github.com/conman1005/ValheimBackupTool
+ *    Time of Last Edit: 2024-06-21 11:14 PM
+ * 
+ *     Form Description: This Form provides a User interface that allows
+ *                       users to select a Character or World to back up
+ *                       from their corrosponding lists.
+ *                       Those lists (ListView) have corrosponding
+ *                       buttons for backing up and managing backups.
+ */
+
+
 namespace ValheimBackup
 {
     public partial class frmMain : Form
     {
-
+        // Initialize Valheim String
         public String urlValheim;
 
-        // World URLs
-        public String urlWorld = "C:/Users/conne/AppData/LocalLow/IronGate/Valheim/worlds_local";
-        public String urlCharacter = "C:/Users/conne/AppData/LocalLow/IronGate/Valheim/characters_local";
+        // Initialize World URLs
+        public String urlWorld;
+        public String urlCharacter;
 
         // Initialize World and Character URLs
         List<String> urlWorlds = new List<String>();
@@ -21,59 +35,79 @@ namespace ValheimBackup
         {
             InitializeComponent();
 
-            //MessageBox.Show(System.IO.Path.GetDirectoryName(Application.ExecutablePath));
+            //MessageBox.Show(System.IO.Path.GetDirectoryName(Application.ExecutablePath));       // Test code to find where program is saved
 
+            // Initialize Initialization file name
             String ini = "./ValheimBackupTool.ini";
+
+            // Use Environment.SpecialFolder.ApplicationData to set the default Valheim save location String
             String defaultLocation = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).Split("Roaming")[0] + "LocalLow\\IronGate\\Valheim";
 
+            // If the ini file happens to be corrupted or deleted during the startup the do while loop will make sure a new ini file is made
             do
             {
                 // Ask user for Valheim Save folder location if ini file does not exist
                 if (File.Exists(ini))
                 {
                     //File.Delete(ini); // Test code for handling a file error when reading
+
+                    // Try to read ini file
                     try
                     {
                         StreamReader readIni = new StreamReader(ini);
+
+                        // Save the First line in the ini file as urlValheim for the save location
                         urlValheim = readIni.ReadLine();
+
+                        // Close File
                         readIni.Close();
 
+                        // Set World/Character folder locations
                         urlWorld = urlValheim + "\\worlds_local";
                         urlCharacter = urlValheim + "\\characters_local";
                     }
                     catch (IOException)
                     {
                         File.Delete(ini);
-                        //MessageBox.Show("Error opening Initiation File. Please restart this program");
+                        //MessageBox.Show("Error opening Initiation File. Please restart this program");    No more need to restart program with this loop... Right???
                     }
-                    //File.Delete(ini);
+                    //File.Delete(ini);     Testing code to quicly remove ini file
                 }
                 else
                 {
+                    // Initialize StreamWriter as outputFile
                     StreamWriter outputFile = new StreamWriter(ini, false);
 
+                    // Check if the default save location for Valheim exists on the computer
                     if (Directory.Exists(defaultLocation))
                     {
                         //MessageBox.Show("Valheim Save Folder was Found");
+
+                        // If default save location exists write it to the ini file
                         using (outputFile)
                         {
                             outputFile.WriteLine(defaultLocation);
                             outputFile.Close();
                         }
-
                     }
                     else
                     {
+                        // If default Valheinm save location is not found then ask user to select the Valheim save location
                         FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
                         folderBrowser.Description = "Please select the folder containing the Valheim Save Data";
                         folderBrowser.UseDescriptionForTitle = true;
                         folderBrowser.InitialDirectory = defaultLocation;
 
+                        // Initialize valid Boolean for while loop to run whilw selected location is invalid
                         bool valid = false;
 
+                        // Run while Valheim save location is invalid
                         while (!valid)
                         {
+                            // Get folderBrowser Result from showing dialog
                             DialogResult result = folderBrowser.ShowDialog();
+
+                            // Close program if user clicks cancel or 'X'
                             if (result != DialogResult.OK)
                             {
                                 MessageBox.Show("You must provide the Valheim Save Locations for this program to work.\nThis program will now close.");
@@ -82,22 +116,25 @@ namespace ValheimBackup
                             }
                             else
                             {
+                                // Set Character and World URLs from the User Selected Path
                                 urlWorld = folderBrowser.SelectedPath + "\\worlds_local";
                                 urlCharacter = folderBrowser.SelectedPath + "\\characters_local";
 
+                                // Check if World/Character folders exist and if the Root of the Save Folder is named 'Valheim'
                                 if (!Directory.Exists(urlWorld) || !Directory.Exists(urlCharacter) || !folderBrowser.SelectedPath.EndsWith("Valheim"))
                                 {
+                                    // Send MessageBox Error message if criteria is not met
                                     MessageBox.Show("This does not seem to be the right folder.\nPlease chose a different folder.\nThe folder should have the name \"Valheim\".");
                                 }
                                 else
                                 {
+                                    // otherwise set valid to true
                                     valid = true;
                                 }
                             }
                         }
 
-                        //File.Create(ini);
-
+                        // Write the Valheim save location to the ini file once the location is valid
                         using (outputFile)
                         {
                             outputFile.WriteLine(folderBrowser.SelectedPath);
@@ -111,19 +148,18 @@ namespace ValheimBackup
                         this.WindowState = FormWindowState.Normal;
                     }
                 }
+                // If the ini file happens to be corrupted or deleted during the startup the do while loop will make sure a new ini file is made
             } while (!File.Exists(ini));
 
             // Load lists with world and character names
-
             String[] worldFolder = Directory.GetFiles(urlWorld);
             String[] characterFolder = Directory.GetFiles(urlCharacter);
 
-
+            // Loop through the World Folder
             foreach (String worldName in worldFolder)
             {
-                //MessageBox.Show(worldName);
-                Name = worldName.Split("\\")[worldName.Split("\\").Length - 1];
-                //Name = worldName;
+                // Get the World Name from the File Name
+                String Name = worldName.Split("\\")[worldName.Split("\\").Length - 1];
 
                 // Check for current world files
                 if (Name.Split("_").Length < 2 && Name.Split(".").Length < 3 && Name.Split(".")[1].Equals("fwl"))
@@ -136,10 +172,11 @@ namespace ValheimBackup
                 }
             }
 
+            // Loop through Character Folder
             foreach (String characterName in characterFolder)
             {
-                Name = characterName.Split("\\")[characterName.Split("\\").Length - 1];
-                //Name = characterName;
+                // Get Character Name from File Name
+                String Name = characterName.Split("\\")[characterName.Split("\\").Length - 1];
 
                 // Check for current world files
                 if (Name.Split("_").Length < 3 && Name.Split(".").Length < 3 && Name.Split(".")[1].Equals("fch"))
@@ -178,6 +215,7 @@ namespace ValheimBackup
             File.Copy(urlWorlds[lstWorld.SelectedIndex], urlWorld + "/Backups/" + lstWorld.SelectedItem + "/" + lstWorld.SelectedItem + "_" + time.ToString("HH-mm-ss MM-dd-yyyy") + ".fwl", false);
             File.Copy(urlWorlds[lstWorld.SelectedIndex].Split(".")[0] + ".db", urlWorld + "/Backups/" + lstWorld.SelectedItem + "/" + lstWorld.SelectedItem + "_" + time.ToString("HH-mm-ss MM-dd-yyyy") + ".db", false);
 
+            // Let user know backup was sucessfull
             MessageBox.Show(lstWorld.SelectedItem.ToString() + " has been Backed Up.");
         }
 
@@ -186,6 +224,7 @@ namespace ValheimBackup
             // Set World save management Forms with type and URL
             mngWorlds = new ManageBackups("Worlds", urlWorld, lstWorld.Items, urlWorlds);
 
+            // Show the World Management Form
             mngWorlds.Show();
         }
 
@@ -197,12 +236,8 @@ namespace ValheimBackup
                 MessageBox.Show("Please select a Character to Back Up.");
                 return;
             }
-
-            /*if (MessageBox.Show("FYI: Loading Character Backups does not work as the game will not add a character unless it is created in game.", "Loading Will Not Work", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.Cancel)
-            {
-                return;
-            }*/
-
+            
+            // Initialize time as the current time
             DateTime time = DateTime.Now;
 
             // Create 'Backups' Directory if it doesn't exist
@@ -229,6 +264,7 @@ namespace ValheimBackup
                 File.Copy(oldURL.Split(".")[0] + ".db", newURL.Split(".")[0] + ".db", false);
             }
 
+            // Let user know backup was sucessfull
             MessageBox.Show(lstCharacter.SelectedItem.ToString() + " has been Backed Up.");
         }
 
@@ -237,6 +273,7 @@ namespace ValheimBackup
             // Set Character save management Forms with type and URL
             mngCharacters = new ManageBackups("Characters", urlCharacter, lstCharacter.Items, urlCharacters);
 
+            // Show the Character Management Form
             mngCharacters.Show();
         }
     }
--- ValheimBackup/ManageBackups.cs
+++ ValheimBackup/ManageBackups.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+ *              Authour: Conner Cullity
+ *               GitHub: @conman1005
+ *            Repo Link: https://github.com/conman1005/ValheimBackupTool
+ *    Time of Last Edit: 2024-06-21 11:14 PM
+ * 
+ *     Form Description: This Form provides a User interface that allows
+ *                       users to select a Character or World to recover.
+ *                       There are Buttons to Delete and to Load Saves.
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +24,22 @@ namespace ValheimBackup
 {
     public partial class ManageBackups : Form
     {
+        // Initialize Management Variables
         public String type;
         public String folderURL;
         public List<String> lstURLs;
         String[] saves;
+
+        /**
+         * <param name="type"/>
+         * <param name="folderURL"/>
+         * <param name="lstNames"/>
+         * <param name="lstURLs"/>
+         * 
+         * 
+         * <summary>The <b>ManageBackups</b> cunstructor is used for determining the backup type (Character or World),<br/> and to get World/Character Names/URLs from the Main Form.</summary>
+         * 
+         */
         public ManageBackups(String type, String folderURL, ListBox.ObjectCollection lstNames, List<String> lstURLs)
         {
             InitializeComponent();
@@ -35,6 +59,7 @@ namespace ValheimBackup
 
         private void lstNames_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // Reset Lists when list is selected with no items selected
             if (lstNames.SelectedItems.Count > 0)
             {
                 ResetSaves();
@@ -51,88 +76,74 @@ namespace ValheimBackup
                 return;
             }
 
+            // Show Confirmation of Recovery Dialog
             DialogResult confirm = MessageBox.Show("Are you sure you want to Load " + lstNames.SelectedItem.ToString() + " saved at " + lstSaves.SelectedItem.ToString() + " ?\nCurrent save file WILL be lost.\nIf you prefer to have a copy of the current save,\nplease go back to the main menu and back up " + lstNames.SelectedItem.ToString() + " to ensure the data won't be lost.", "Confirm Recovery", MessageBoxButtons.YesNo);
 
+            // Stop recovery of user does not select Yes
             if (confirm != DialogResult.Yes)
             {
                 return;
             }
 
-            //DateTime time = DateTime.Now;
-
-            /*// Create 'Backups' Directory if it doesn't exist
-            if (!Directory.Exists(folderURL + "/Backups"))
-            {
-                Directory.CreateDirectory(folderURL + "/Backups");
-            }*/
-
-
-
-            String path = lstURLs[lstNames.SelectedIndex]/*.Split("\\")[lstURLs[lstSaves.SelectedIndex].Split("\\").Length - 3] + "\\"*/;
-            String nameFWL = path/* + lstURLs[lstSaves.SelectedIndex].Split("\\")[lstURLs[lstSaves.SelectedIndex].Split("\\").Length - 1]*/;
+            // Set FWL (or FCH) file and corrosponding DB file names
+            String nameFWL = lstURLs[lstNames.SelectedIndex];
             String nameDB = nameFWL.Split(".")[0] + ".db";
 
 
-            //foreach (String save in saves)
+            // Loop through saves
             for (int i = 0; i < saves.Length; i++)
             {
+                // Run if save is found
                 String[] saveName = lstSaves.SelectedItem.ToString().Split(" ");
                 if (saves[i].Contains(saveName[0].Replace(":", "-") + " " + saveName[1]))
                 {
-                    //MessageBox.Show(saves[i]);
+                    // Recover DB file when found
                     if (saves[i].Contains(".db"))
                     {
-                        
-                        
                         File.Copy(saves[i], nameDB, true);
                     }
                     else
                     {
+                        // Otherwise recocover fch or fwl
                         File.Copy(saves[i], nameFWL, true);
                     }
                 }
             }
 
-
+            // Let User know that the recovery was Sucessful
             MessageBox.Show(lstSaves.SelectedItem.ToString() + " Has Been Recovered Sucessfully");
-
-
-
-            // Copy files to a main folder
-            //File.Copy(path, nameFWL, true);
-            //File.Copy(".db", true);
-
-            //MessageBox.Show(lstWorld.SelectedItem.ToString() + " has been Backed Up.");
         }
 
         private void btnDelete_Click(object sender, EventArgs e)
         {
-            //saves = Directory.GetFiles(folderURL + "/Backups/" + lstNames.SelectedItem.ToString());
-
+            // Show confirmatiom of deletion dialog
             DialogResult confirm = MessageBox.Show("Are you sure you want to delete this?", "Confirm Deletion", MessageBoxButtons.YesNo);
 
+            // Stop if user selects anything other than yes
             if (confirm != DialogResult.Yes)
             {
                 return;
             }
 
+            // Make sure a save is selected, return if not
             if (lstSaves.SelectedItems.Count == 0)
             {
                 MessageBox.Show("Please select a Save to Delete.");
                 return;
             }
 
+            // Loop through saves
             foreach (String save in saves)
             {
+                // Delete save if it matches the selected save
                 String[] saveName = lstSaves.SelectedItem.ToString().Split(" ");
-                //MessageBox.Show(save);
                 if (save.Contains(saveName[0].Replace(":", "-") + " " + saveName[1]))
                 {
                     File.Delete(save);
                 }
             }
 
-
+            // Let user know save was deleted sucessfully
             MessageBox.Show(lstSaves.SelectedItem.ToString() + " Has Been Deleted Sucessfully");
             ResetSaves();
         }
